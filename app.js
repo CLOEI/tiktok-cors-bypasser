15bdaef1a41e1b5ed167a1f52f27b47581fc4b3e
Cendy <cendyemail@gmail.com>
2021-05-14 14:27:07 +0700

    Bypassed shortened link

--- app.js
+++ app.js
@@ -11,10 +11,16 @@ app.use(cors());
 
 
 app.get('/bypass', async (req, res) => {
-    if (req.query.url == null) return res.sendStatus('403');
     const cookieJar = new CookieJar();
+    let link = req.query.url;
 
-    got(req.query.url, {
+    if (req.query.url == null) return res.sendStatus('403');
+    if (!req.query.url.startsWith('https://www')) {
+        const newData = await got(req.query.url);
+        link = newData.redirectUrls[0].replace('https://t', 'https://www');
+    }
+
+    got(link, {
         headers: {
             'User-Agent': 'Mozilla/5.0 (Linux; Android 9; SM-G950F Build/PPR1.180610.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/74.0.3729.157 Mobile Safari/537.36'
         },
